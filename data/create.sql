1bee67c53995e3d286c7e6005b47b120cc6b7bc6
DE ANGELIS Loick <deangell@hostname.iut2.upmf-grenoble.fr>
2018-11-08 19:48:33 +0100

    try

--- data/create.sql
+++ data/create.sql
@@ -1,11 +1,9 @@
-DROP TABLE Voiture;
-
 CREATE TABLE Voiture (
   reference integer,
   nom varchar(60),
   marque varchar(60),
   modele varchar(60),
   annee integer,
-  img varchar(100)
+  img varchar(100),
   PRIMARY KEY(reference)
 );
