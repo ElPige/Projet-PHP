428a075be129cf6678482c85776cfa03fa3072e4
ElPige <daloick@gmail.com>
2018-11-08 19:02:11 +0100

    push

---
+++ data/create.sql
@@ -0,0 +1,11 @@
+DROP TABLE Voiture;
+
+CREATE TABLE Voiture (
+  reference integer,
+  nom varchar(60),
+  marque varchar(60),
+  modele varchar(60),
+  annee integer,
+  img varchar(100)
+  PRIMARY KEY(reference)
+);
