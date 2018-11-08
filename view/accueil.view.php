560db8413e57ae58e5a1bb5e63f2202ab77b5ffa
Lleiro <Lleiro38@gmail.com>
2018-11-08 18:59:27 +0100

    Création accueil controler/view

---
+++ view/accueil.view.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Page d'accueil</title>
+  </head>
+  <body>
+    <header>
+      <h1>CAR'USELL</h1>
+
+      <link rel="stylesheet" type="text/css" href="css/accueil.css">
+    </header>
+    <div class="Descriptif">
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
+
+    <div class="Connection/Inscription">
+      <h2>CONNNECTEZ VOUS</h2>
+      <fieldset>
+      <legend align="left"><div id="fieldtitre"> Connexion</div></legend>
+      <form action="main.ctrl.php">
+        <p>
+          Adresse email <input name="mail" type="text" size=25 > <br>
+          Mot de passe <input name="mdp" type="password" size=25> <br>
+          <input type="submit" value="Connexion"/>
+        </p>
+      </form>
+
+
+      </fieldset>
+
+      <a href="http://www-etu-info.iut2.upmf-grenoble.fr/~deangell/ProgWeb/tp6-projet/view/inscription.php"> S'inscrire</a>
+    </div>
+
+    <div class="Annonces">
+      <p>ici y'aura les annonces Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
+
+    <footer>
+      <a href="https://www.google.com">Contactez nous</a>
+      <a href="https://www.google.com/intl/fr_fr/about/?utm_source=google.com&utm_medium=referral&utm_campaign=hp-footer&fg=1">Qui sommes-nous ?</a>
+    </footer>
+  </body>
+</html>
