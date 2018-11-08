b03fc8d3ec6c0b3971c9075b876fad9763a93e98
ElPige <daloick@gmail.com>
2018-11-08 18:38:03 +0100

    re

--- view/main.view.php
+++ view/main.view.php
@@ -8,24 +8,6 @@
     <?php
     print "<h2> Annonces de voiture </h2>";
     print '</br>';
-    print "<div class = 'conteneur'>";
-    foreach ($acoustique as $key => $value) {
-      print '<article id="guitares">';
-      // si l'utilisateur est connecté alors il a la possibilité d'ajouter des produits à ses favoris en cliquant sur l'image
-      if (isset($_SESSION['pseudo'])){
-          print "<div class = 'img'><a href=\"../controler/ajoutProduit.ctrl.php?categorie=acoustique&id=$value->id\"><img src = \"../data/BD/$value->img\"/></a></div>";
-      }
-      else{
-          print "<div class = 'img'><img src = \"../data/BD/$value->img\"/></div>";
-      }
-        print "<div class = 'text'><h3> $value->nom </h3>";
-        if($value->electroacoustique =='oui'){
-          print "<p> Electro-Acoustique <p>";
-        }
-        print "<p> • Prix : $value->prix  </p></div>";
-      print '</article>';
-    }
-    print "</div>";
   ?>
   </body>
 </html>
