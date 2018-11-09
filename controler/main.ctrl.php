e4f9e4ff870316fc6980adf599743ed4a335542c
DE ANGELIS Loick <deangell@hostname.iut2.upmf-grenoble.fr>
2018-11-09 10:10:29 +0100

    update

--- controler/main.ctrl.php
+++ controler/main.ctrl.php
@@ -11,6 +11,8 @@ if (isset($_GET['annonce'])) {
 
 if ($annonce != '') {
     $annoncev = $dao->getAnnonce($annonce);
+  $id = $annoncev->appartienRef;
+  $m = $dao->getMember($id);
   include('../view/annonce.view.php');
 } else {
   include('../view/main.view.php');
