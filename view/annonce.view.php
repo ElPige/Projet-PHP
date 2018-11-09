1900a07f01bb6f91747beec4a5ddf2ab03869096
MONTEIRO Leo <monteile@hostname.iut2.upmf-grenoble.fr>
2018-11-09 07:55:45 +0100

    Flèche retour

--- view/annonce.view.php
+++ view/annonce.view.php
@@ -5,11 +5,14 @@
     <title></title>
   </head>
   <body>
-
     <?php
+
+      $retour = 'http://www-info.iut2.upmf-grenoble.fr/intranet/enseignements/ProgWeb/M3104/TP/tp02/sujet/img/Actions-arrow-right-icon.png';
+      echo '<a href="../controler/main.ctrl.php"><img src='.$retour.'></a>';
+
       $numAnnonce = $_GET["annonce"];
 
-      
+
 
 
      ?>
