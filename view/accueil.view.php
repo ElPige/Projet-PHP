f2cf3f2d53d35810dc1d3da30dc8b90224a32c52
ElPige <daloick@gmail.com>
2018-11-09 18:03:36 +0100

    update

--- view/accueil.view.php
+++ view/accueil.view.php
@@ -33,9 +33,6 @@
       <a href="../controler/inscription.ctrl.php"> S'inscrire</a>
     </div>
 
-    <div class="Annonces">
-      <p>ici y'aura les annonces Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-    </div>
 
     <footer>
 
