3d65b824f8f66d17d3a78058dbc415bd6cc57155
MONTEIRO Leo <monteile@hostname.iut2.upmf-grenoble.fr>
2018-11-09 09:49:50 +0100

    updated

--- view/ajouterAnnonce.view.php
+++ view/ajouterAnnonce.view.php
@@ -11,16 +11,16 @@
    <form method="post" action="../controler/main.ctrl.php" enctype="multipart/form-data">
 
    <fieldset><legend>Créer une annonce</legend>
-   <label for="Titre"> Titre * :</label>  <input name="pseudo" type="text" id="pseudo" /><br />
-   <label for="Descriptif"> Descriptif * :</label><input type="password" name="password" id="password" /><br />
+   <label for="Titre"> Titre * :</label>  <input name="pseudo" type="text" id="Titre" required/><br />
+   <label for="Descriptif"> Descriptif * :</label><textarea name="Descriptif" rows="8" cols="80" required></textarea>
    </fieldset>
 
    <fieldset><legend>Données voiture</legend>
-   <label for="nom"> Nom * :</label><input type="text" name="nom" id="nom" /><br />
-   <label for="marque"> Marque * :</label><input type="text" name="marque" id="marque" /><br />
-   <label for="Modèle"> Modèle * :</label><input type="text" name="Modèle" id="Modèle" /><br />
-   <label for="Année"> Année * :</label><input type="text" name="Année" id="Année" /><br />
-   <label for="Prix"> Prix * :</label><input type="text" name="Prix" id="Prix" /><br />
+   <label for="nom"> Nom * :</label><input type="text" name="nom" id="nom" required/><br />
+   <label for="marque"> Marque * :</label><input type="text" name="marque" id="marque" required/><br />
+   <label for="Modèle"> Modèle * :</label><input type="text" name="Modèle" id="Modèle" required/><br />
+   <label for="Année"> Année * :</label><input type="text" name="Année" id="Année" required/><br />
+   <label for="Prix"> Prix * :</label><input type="text" name="Prix" id="Prix" required/><br />
    </fieldset>
 
    <p>Les champs suivis d'un * sont obligatoires</p>
