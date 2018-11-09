cd9b9970d4578312e3a9cfd483ac572d088ed1e8
MONTEIRO Leo <monteile@hostname.iut2.upmf-grenoble.fr>
2018-11-09 08:13:29 +0100

    maj mdp

--- model/Membre.php
+++ model/Membre.php
@@ -6,7 +6,8 @@ class Membre
   public $reference;
   public $nom;
   public $prenom;
-  public $pseudo;
+  public $pseudo; // identifiant de connection;
+  public $motDePasse;
   public $email;
   public $telephone;
 };
